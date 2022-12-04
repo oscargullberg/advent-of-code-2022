fd0d1378fe7c135540ae46ec4fbb514aca1c91b6
Oscar Gullberg <oscar@filestar.com>
2022-12-04 20:39:59 +0100

    Minor refactor

--- lib/days/day4.ex
+++ lib/days/day4.ex
@@ -3,22 +3,16 @@ defmodule AOC2022.Day4 do
   def part_1(input) do
     input
     |> create_ranges()
-    |> Stream.map(fn
-      {r1 = l1..u1, r2 = l2..u2} when l1 <= l2 and u1 >= u2 -> {r1, r2, true}
-      {r1 = l1..u1, r2 = l2..u2} when l2 <= l1 and u2 >= u1 -> {r1, r2, true}
-      {r1, r2} -> {r1, r2, false}
-    end)
-    |> Enum.count(fn {_, _, contains_other} -> contains_other end)
+    |> Stream.filter(fn {l1..u1, l2..u2} -> (l1 <= l2 and u1 >= u2) or (l2 <= l1 and u2 >= u1) end)
+    |> Enum.count()
   end
 
   @spec part_2(File.Stream.t()) :: number()
   def part_2(input) do
     input
     |> create_ranges()
-    |> Stream.map(fn
-      {r1, r2} -> {r1, r2, !Range.disjoint?(r1, r2)}
-    end)
-    |> Enum.count(fn {_, _, overlaps} -> overlaps end)
+    |> Stream.filter(fn {r1, r2} -> !Range.disjoint?(r1, r2) end)
+    |> Enum.count()
   end
 
   defp create_ranges(input) do
